6cca001a8b308e1a6fa6887cf0af7c5174ff78b1
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2015-04-14 11:37:32 +1000

    Added a README with example files

---
+++ gulpfile.example.js
@@ -0,0 +1,10 @@
+/**
+ * Milkyway Multimedia
+ * Automatic asset handling
+ *
+ * @package milkyway-multimedia/assets
+ * @author Mellisa Hankins <mell@milkywaymultimedia.com.au>
+ */
+
+var gulp = require('gulp');
+require('mwm-buildr')(gulp);
