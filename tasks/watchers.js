78ebb06dc21c8bea9dcfee0897b4e9074e5d9777
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2015-05-21 05:04:55 +1000

    Watchers should only watch modules that change, not rebuild whole project each time

--- tasks/watchers.js
+++ tasks/watchers.js
@@ -43,9 +43,10 @@ module.exports = function (gulp, $, $env) {
 
         for (var task in watchTasks) {
             if (watchTasks.hasOwnProperty(task)) {
-                var src = [];
-
                 configurations.forEach(function (configuration) {
+                    var src = [],
+                        taskName = configuration.hasOwnProperty('moduleFolder') ? configuration.moduleFolder + ':' + task : task;
+
                     if (configuration.hasOwnProperty(task)) {
                         if (Array.isArray(configuration[task])) {
                             configuration[task].forEach(function (minorTask) {
@@ -64,11 +65,19 @@ module.exports = function (gulp, $, $env) {
                             src = src.concat(configuration[task].src);
                         }
                     }
-                });
 
-                if (src.length) {
-                    watchers[task] = gulp.watch(src, watchTasks[task]);
-                }
+                    if (src.length) {
+                        watchers[taskName] = gulp.watch(src, function(tasks, config){
+                            return function() {
+                                $env.set('configuration_override', config);
+
+                                $helpers.sequence(tasks, function() {
+                                    $env.set('configuration_override', null);
+                                });
+                            };
+                        }(watchTasks[task], configuration));
+                    }
+                });
             }
         }
 
@@ -76,30 +85,28 @@ module.exports = function (gulp, $, $env) {
             watchers['refresh'] = gulp.watch($env.project().refresh, ['server:reload']);
         }
 
-        if (Object.keys(watchers).length) {
-            for (var key in watchers) {
-                if (watchers.hasOwnProperty(key)) {
-                    (
-                        function (name) {
-                            watchers[key].on('change', function (event) {
-                                $.util.beep();
-                                $.util.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        for (var key in watchers) {
+            if (watchers.hasOwnProperty(key)) {
+                (
+                    function (name) {
+                        watchers[key].on('change', function (event) {
+                            $.util.beep();
+                            $.util.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 
-                                if (event.type === 'deleted') {
-                                    if ($.cached.caches.hasOwnProperty(name) && $.cached.caches[name].hasOwnProperty(event.path)) {
-                                        delete $.cached.caches[name][event.path];
-                                        $.remember.forget(name, event.path);
-                                    }
-
-                                    if (name === 'js' && $.cached.caches.hasOwnProperty('js:lint') && $.cached.caches['js:lint'].hasOwnProperty(event.path)) {
-                                        delete $.cached.caches['js:lint'][event.path];
-                                        $.remember.forget('js:lint', event.path);
-                                    }
+                            if (event.type === 'deleted') {
+                                if ($.cached.caches.hasOwnProperty(name) && $.cached.caches[name].hasOwnProperty(event.path)) {
+                                    delete $.cached.caches[name][event.path];
+                                    $.remember.forget(name, event.path);
                                 }
-                            });
-                        }(key)
-                    );
-                }
+
+                                if (name === 'js' && $.cached.caches.hasOwnProperty('js:lint') && $.cached.caches['js:lint'].hasOwnProperty(event.path)) {
+                                    delete $.cached.caches['js:lint'][event.path];
+                                    $.remember.forget('js:lint', event.path);
+                                }
+                            }
+                        });
+                    }(key)
+                );
             }
         }
     });
