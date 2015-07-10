e0f47561f3d59d0a27b007989043121a24596832
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2015-07-10 15:35:26 +1000

    Build tasks can now be overwritten in assets.json

--- tasks/build.js
+++ tasks/build.js
@@ -1,21 +1,25 @@
 module.exports = function (gulp, $, $env) {
     var $helpers = require("../lib/helpers")(gulp, $, $env),
-        assetTasks = {
-            'build:css':     ['css:single', 'css:merged'],
-            'build:js':      ['js:single', 'js:merged'],
-            'build:html':    ['html'],
-            'build:images':  ['images'],
-            'build:sprites': ['sprites'],
-            'build:copy':    ['copy']
+        tasks = {
+            assets: {
+                'build:css':     ['css:single', 'css:merged'],
+                'build:js':      ['js:single', 'js:merged'],
+                'build:html':    ['html'],
+                'build:images':  ['images'],
+                'build:sprites': ['sprites'],
+                'build:copy':    ['copy']
+            },
+            build: [
+                'build:css',
+                'build:js',
+                'build:html',
+                'build:images',
+                'build:sprites',
+                'build:copy'
+            ]
         },
-        buildTasks = [
-            'build:css',
-            'build:js',
-            'build:html',
-            'build:images',
-            'build:sprites',
-            'build:copy'
-        ],
+
+        buildTasksHasBeenReset = false,
 
         syncOrNot = function (done) {
             var callback = function () {
@@ -30,14 +34,65 @@ module.exports = function (gulp, $, $env) {
             else {
                 $helpers.sequence('jekyll', callback);
             }
+        },
+
+        startAssetTasks = function() {
+            for (var task in tasks.assets) {
+                if (tasks.assets.hasOwnProperty(task)) {
+                    gulp.task(task, tasks.assets[task], syncOrNot);
+                }
+            }
+        },
+
+        // Build all assets
+        startBuildTasks = function() {
+            var buildTasks = tasks.build;
+
+            if(buildTasks.indexOf('start') === -1)
+                buildTasks.unshift('start');
+
+            gulp.task('build', buildTasks);
+
+            return true;
+        }
+
+        resetGulp = function() {
+            gulp.on('stop', function () {
+                if(buildTasksHasBeenReset === false) {
+                    var commandLineTasks = $.util.env._.length ? $.util.env._ : ['default'];
+                    gulp.start(commandLineTasks);
+                }
+                buildTasksHasBeenReset = true;
+            });
+
+            gulp.stop(false, true);
         };
 
-    for (var task in assetTasks) {
-        if (assetTasks.hasOwnProperty(task)) {
-            gulp.task(task, assetTasks[task], syncOrNot);
-        }
-    }
+    $env.on('start', function(env, configs) {
+        var task;
 
-    // Build all assets
-    gulp.task('build', buildTasks);
+        if(!env.hasOwnProperty('tasks'))
+            return;
+
+        if(env.tasks.hasOwnProperty('assets')) {
+            for (task in env.tasks.assets) {
+                if (env.tasks.assets.hasOwnProperty(task)) {
+                    tasks.assets[task] = env.tasks.assets[task];
+                }
+            }
+            startAssetTasks();
+        }
+
+        if(env.tasks.hasOwnProperty('build')) {
+            tasks.build = env.tasks.build;
+            startBuildTasks();
+        }
+
+        if(env.tasks.hasOwnProperty('assets') || env.tasks.hasOwnProperty('build')) {
+            resetGulp();
+        }
+    });
+
+    startAssetTasks();
+    startBuildTasks();
 };
