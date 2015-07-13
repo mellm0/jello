79b593d157e42e723cef7ac7cc99de8c97156d5f
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2015-07-14 00:07:08 +1000

    FIX: Added fix for errors no longer breaking pipes

--- index.js
+++ index.js
@@ -12,6 +12,32 @@ module.exports = function (gulp, tasks) {
         gulp = require('gulp');
     }
 
+    // Some fixes to gulp for handling errors
+    // @TODO: To be removed in Gulp 4
+    var origSrc = gulp.src;
+    gulp.src = function () {
+        return fixPipe(origSrc.apply(this, arguments));
+    };
+    function fixPipe(stream) {
+        var origPipe = stream.pipe;
+        stream.pipe = function (dest) {
+            arguments[0] = dest.on('error', function (error) {
+                var nextStreams = dest._nextStreams;
+                if (nextStreams) {
+                    nextStreams.forEach(function (nextStream) {
+                        nextStream.emit('error', error);
+                    });
+                } else if (dest.listeners('error').length === 1) {
+                    throw error;
+                }
+            });
+            var nextStream = fixPipe(origPipe.apply(this, arguments));
+            (this._nextStreams || (this._nextStreams = [])).push(nextStream);
+            return nextStream;
+        };
+        return stream;
+    }
+
     if (!tasks) {
         tasks = [
             './tasks/watchers',
