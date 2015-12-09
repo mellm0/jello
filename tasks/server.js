d0b12ff68ee60dd444180b42f28af8ecb32641fd
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2015-12-10 04:51:02 +1100

    Allow local php server

--- tasks/server.js
+++ tasks/server.js
@@ -1,13 +1,37 @@
 module.exports = function (gulp, $, $env) {
-    var $defaults = require("../lib/defaults")(gulp, $, $env);
+    var $defaults = require("../lib/defaults")(gulp, $, $env),
+        startPhp = function(options, cb) {
+            if(options && (!options.hasOwnProperty('php') || !$env.shell.which('php'))) {
+                return;
+            }
+
+            var host = options.php !== true && options.php.hasOwnProperty('host') ? options.php.host : $defaults.php.host,
+                ini = options.php !== true && options.php.hasOwnProperty('ini') ? ' -c ' + options.php.ini : '',
+                dir = options.php !== true && options.php.hasOwnProperty('dir') ? ' -t ' + options.php.dir : '',
+                router = options.php !== true && options.php.hasOwnProperty('router') ? ' ' + options.php.router : '';
+
+            $env.shell.exec('php -S ' + host + ini + dir + router, {async: true}, function() {
+                if(cb) {
+                    cb();
+                }
+            });
+        };
 
     // Start a browser sync server
     gulp.task('server', ['jekyll'], function (done) {
         var options = $env.project().hasOwnProperty('server') ? $env.project().server : $defaults.server.options;
 
+        startPhp(options);
+
         $env.server(options, function () {
             done();
         });
+    });
+
+    // Start a php server
+    gulp.task('server:php', ['jekyll'], function (done) {
+        var options = $env.project().hasOwnProperty('server') ? $env.project().server : $defaults.server.options;
+        startPhp(options, done);
     });
 
     // Reload server
