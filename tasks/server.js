87ed4633b9d6bd2e363b0267e17b7b49e98047c8
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2016-07-06 15:34:30 +1000

    FIX: Add the ability to use caddy

--- tasks/server.js
+++ tasks/server.js
@@ -17,16 +17,44 @@ module.exports = function (gulp, $, $env) {
             });
 
             return true;
+        },
+        startCaddy = function(options, cb) {
+            if(options && (!options.hasOwnProperty('caddy') || !$env.shell.which('caddy'))) {
+                return false;
+            }
+
+            var confFile = options.caddy !== true && options.caddy.hasOwnProperty('configuration_file') ? ' -conf=' + options.caddy.configuration_file : '',
+                logTo = options.caddy !== true && options.caddy.hasOwnProperty('log_to') ? ' -log=' + options.caddy.log_to : '',
+                port = options.caddy !== true && options.caddy.hasOwnProperty('port') ? ' -port ' + options.caddy.port : '',
+                root = options.caddy !== true && options.caddy.hasOwnProperty('root') ? ' -root=' + options.caddy.root : '';
+
+            $env.shell.exec('caddy ' + confFile + logTo + port + root, {async: true}, function() {
+                if(cb) {
+                    cb();
+                }
+            });
+
+            return true;
         };
 
     // Start a browser sync server
     gulp.task('server', ['jekyll'], function (done) {
         var options = $env.project().hasOwnProperty('server') ? $env.project().server : $defaults.server.options,
-            isPhp = startPhp(options);
+            isPhp = startPhp(options),
+            isCaddy = startCaddy(options);
 
         if(isPhp) {
             options.proxy = options.php !== true && options.php.hasOwnProperty('host') ? options.php.host : $defaults.php.host;
             options.open = true;
+        }
+        else if(isCaddy) {
+            if(options.caddy === true) {
+                options.proxy = '127.0.0.1:2015';
+            }
+            else {
+                options.proxy = options.caddy.hasOwnProperty('bind_to') ? options.caddy.bind_to : '127.0.0.1';
+                options.proxy += ':' + (options.caddy.hasOwnProperty('port') ? options.caddy.port : '2015');
+            }
         }
 
         if(options.hasOwnProperty('proxy')) {
